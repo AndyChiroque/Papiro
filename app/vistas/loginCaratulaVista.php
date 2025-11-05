bc969f757c7f0709938de552a5a8adcaa2ae2b67
Andy Chiroque <andyalessandrochiroqueadanaque@gmail.com>
2025-11-05 18:56:41 -0500

    Refactor login view to include Bootstrap navbar and improve layout structure

--- app/vistas/loginCaratulaVista.php
+++ app/vistas/loginCaratulaVista.php
@@ -4,21 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Papiro | Entrada</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+
 </head>
 <body>
-    <h2>Biblioteca</h2>
-    <table>
-        <tr>
-            <td>usuario</td>
-            <td><input type="text" name="usuario" id="usuario"/></td>
-        </tr>
-        <tr>
-            <td>Clave de accesso</td>
-            <td><input type="passsword" name="clave" id="clave"/></td>
-        </tr>
-        <tr>
-            <td><input type="submit"/></td>
-        </tr>
-    </table>
+    <nav class= "navbar navbar-expand-sm bg-dark navbar-dark">
+        <a href="#" class="navbar-brand">Biblioteca</a>
+    </nav>
+    
+    <div class="container-fluid">
+        <div class="row content">
+            <div class="col-sm-1"></div>
+            <div class="col-sm-10">
+                <h2>Login</h2>
+                <table>
+                    <tr>
+                        <td>usuario</td>
+                        <td><input type="text" name="usuario" id="usuario"/></td>
+                    </tr>
+                    <tr>
+                        <td>Clave de accesso</td>
+                        <td><input type="passsword" name="clave" id="clave"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit"/></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
