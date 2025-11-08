6b46dea81bcc5325da6833ab3278888db33f7e65
Andy Chiroque <andyalessandrochiroqueadanaque@gmail.com>
2025-11-08 13:26:48 -0500

    Fix spacing in login header for improved readability

--- app/vistas/loginCaratulaVista.php
+++ app/vistas/loginCaratulaVista.php
@@ -17,7 +17,7 @@
         <div class="row content">
             <div class="col-sm-1"></div>
             <div class="col-sm-10">
-                <h2>Login</h2>
+                <h2> Login </h2>
                 <table>
                     <tr>
                         <td>usuario</td>
