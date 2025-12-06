ca47ee88beb5179112b73ace06e8601a02d45cac
Andy Chiroque <andyalessandrochiroqueadanaque@gmail.com>
2025-12-06 00:23:47 -0500

    Refactorizar la lógica de recuperación de contraseña y agregar vista de mensajes para notificaciones de usuario

--- app/controladores/Login.php
+++ app/controladores/Login.php
@@ -1,51 +1,70 @@
-<?php
+<?php  
 /**
  * 
  */
-class Login extends controlador{
-    //code
-    private $modelo = "";
-    function __construct()
-    {
-        //code
-        $this->modelo = $this->modelo("LoginModelo");
-    }
-    public function caratula(){
-        $datos = [
-            "titulo" => "Sistema de Biblioteca - Login",
-            "subtitulo" => "Sistema de Biblioteca"
-        ];
-        $this->vista("LoginCaratulaVista", $datos);
-    }   
-    public function olvidoVerificar(){
+class Login extends Controlador
+{
+	private $modelo = "";
+	
+	function __construct()
+	{
+		$this->modelo = $this->modelo("LoginModelo");
+	}
 
-        $errores = [];
-        if ($_SERVER['REQUEST_METHOD']=="POST"){
-            $usuario = $_POST['usuario']??"";
-            if (empty($usuario)){
-                array_push($errores, "El campo usuario es obligatorio");
-            }
-            if (filter_var($usuario, FILTER_VALIDATE_EMAIL) === false){
-                array_push($errores, "El campo usuario debe ser un correo electrónico válido");
-            }
-            if (empty($errores)){
-            if ($this->modelo->buscarCorreo($usuario)){
-                if ($this->modelo->enviarCorreo($usuario)){
-                    helper::mostrar("Se ha enviado un correo para restablecer la contraseña", "alert alert-success");
-                }
-               helper::mostrar("Se ha enviado un correo para restablecer la contraseña", "alert alert-success");
-            } else {
-               helper::mostrar("El correo electrónico no está registrado", "alert alert-danger");
-            }
-            }
-        }
-        $datos = [
+	public function caratula()
+	{
+		$datos = [
+			"titulo" => "Entrada a la biblioteca",
+			"subtitulo" => "Sistema de biblioteca"
+		];
+		$this->vista("loginCaratulaVista",$datos);
+	}
+
+	public function olvidoVerificar()
+	{
+		$errores = [];
+		if ($_SERVER['REQUEST_METHOD']=="POST") {
+			$usuario = $_POST['usuario']??"";
+			if (empty($usuario)) {
+				array_push($errores, "El correo electrónico es requerido.");
+			}
+			if (filter_var($usuario,FILTER_VALIDATE_EMAIL)==false) {
+				array_push($errores, "El correo electrónico no está bien escrito.");
+			}
+			if (empty($errores)) {
+				//
+				if ($this->modelo->buscarCorreo($usuario)) {
+					if (!$this->modelo->enviarCorreo($usuario)) {
+						$datos = [
+							"titulo" => "Cambio de clave de acceso",
+							"menu" => false,
+							"errores" => [],
+							"data" => [],
+							"subtitulo" => "Cambio de clave de acceso",
+							"texto" => "Se ha enviado un correo a <b>".$usuario."</b> para que puedas cambiar tu clave de acceso. Cualquier duda te puedes comunicar con nosotros. No olvides revisar tu bandeja de spam.",
+							"color" => "alert-warning",
+							"url" => "login",
+							"colorBoton" => "btn-warning",
+							"textoBoton" => "Regresar"
+						];
+						$this->vista("mensaje",$datos);
+						exit;
+					} else {
+						Helper::mostrar("No se envió el correo");
+					}
+				} else {
+					Helper::mostrar("No se encontró el correo");
+				}
+			}
+		}
+		$datos = [
 			"titulo" => "Olvido de contraseña",
 			"subtitulo" => "¿Olvidaste la contraseña?",
 			"errores" => $errores,
 			"data" => []
-        ];
-        $this->vista("loginOlvidoVista", $datos);
-    }  
+		];
+		$this->vista("loginOlvidoVista",$datos);
+	}
 }
+
 ?>
--- app/modelos/LoginModelo.php
+++ app/modelos/LoginModelo.php
@@ -1,46 +1,49 @@
-<?php
+<?php  
 /**
  * 
  */
-class LoginModelo{
+class LoginModelo
+{
+	private $db="";
+	
+	function __construct()
+	{
+		$this->db = new MySQLdb();
+	}
 
-    private $db = "";
-    //code
-    function __construct(){
-        //code
-        $this->db = new MySQLdb();
-    }
+	public function buscarCorreo($usuario='')
+	{
+		if(empty($usuario)) return false;
+		$sql = "SELECT * FROM usuarios WHERE correo='".$usuario."'";
+		return $this->db->query($sql);
+	}
 
-    public function buscarCorreo($usuario=''){
-        if(empty($usuario)){
-            return false;
-        }
-        $sql = "SELECT * FROM usuarios WHERE correo= '" .$usuario. "'";
-        return $this->db->query($sql);
-    }
+	public function enviarCorreo($email='')
+	{
+		$data = [];
+		if ($email=="") {
+			return false;
+		} else {
+			$data = ["id"=>1]; //$this->validarCorreo($email);
+			if (!empty($data)) {
+				$id = $data["id"];
+				//
+				$msg = "Entra a la siguiente liga para cambiar tu clave de acceso al sistema de biblioteca...<br>";
+				$msg.= "<a href='".RUTA."login/cambiarclave/".$id."'>Cambiar tu clave de acceso</a>";
 
-    public function enviarCorreo($email=''){
-        $data = [];
-        if ($email==""){
-            return false;
-        } else {
-            $data = ["id"=>1]; 
-            if (!empty($data)){
-                $id = $data['id'];
-                //
-                $msg = "Hola, para restablecer tu contraseña, haz clic en el siguiente enlace: \n\n";
-                $msg .= "<a href='".RUTA."login/cambiarclave/$id'>Restablecer Contraseña</a>\n\n";
-                $headers = "MIME-Version: 1.0" . "\r\n";
-                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-                $headers .= "From: Papiro\r\n";
-                $asunto = "Cambiar clave de acceso";
-                var_dump($msg);
-                //return true;
-                return @mail($email, $asunto, $msg, $headers);
-            } else {
-                
-            }
-        }
-    }
+				$headers = "MIME-Version: 1.0\r\n"; 
+				$headers.= "Content-type:text/html; charset=UTF-8\r\n"; 
+				$headers.= "From: Biblioteca\r\n"; 
+				$headers.= "Reply-to: ayuda@biblioteca.com\r\n";
+
+				$asunto = "Cambiar clave de acceso";
+				//var_dump($msg);
+				//return true;
+				return @mail($email,$asunto,$msg, $headers);
+			} else {
+			}
+		}
+	}
 }
+
 ?>
